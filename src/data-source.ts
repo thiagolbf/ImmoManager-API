6a4fe18006c39c3f5a69ee54c6478fd283af3ac9
Thiago Barcelos <thiagolbf@hotmail.com>
2023-08-07 21:47:47 -0300

    build project

---
+++ src/data-source.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import "dotenv/config";
+import path from "path";
+import { DataSource, DataSourceOptions } from "typeorm";
+
+const dataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath = path.join(__dirname, "./entities/**.{ts,js}");
+  const migrationsPath = path.join(__dirname, "./migrations/**.{ts,js}");
+
+  const dbUrl: string | undefined = process.env.DATABASE_URL;
+
+  if (!dbUrl) {
+    throw new Error("Missing env var: 'DATABASE_URL'");
+  }
+
+  return {
+    type: "postgres",
+    url: dbUrl,
+    logging: true,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+};
+
+const AppDataSource = new DataSource(dataSourceConfig());
+
+export { AppDataSource };
