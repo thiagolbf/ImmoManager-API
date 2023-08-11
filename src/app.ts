3bd874113d7a2a668575c9deb8bed98ec97f0c77
Thiago Barcelos <thiagolbf@hotmail.com>
2023-08-10 21:47:04 -0300

    extend error class

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import "express-async-errors";
+import { handleErrors } from "./errors";
 
 import express, { Application, json } from "express";
 
 const app: Application = express();
 app.use(json());
 
+app.use(handleErrors);
 export default app;
