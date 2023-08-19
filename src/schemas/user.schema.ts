d12aadf298630e6a30af5d9ff5e53017c5be5b8c
Thiago Barcelos <thiagolbf@hotmail.com>
2023-08-19 07:48:49 -0300

    add user schema

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const userSchema = z.object({
+  id: z.number().positive(),
+  name: z.string().max(50),
+  email: z.string().max(45),
+  password: z.string().max(120),
+  admin: z.boolean().default(false),
+});
+
+const createUserSchema = userSchema.omit({ id: true });
+
+const updateUserSchema = userSchema.partial();
+
+export { userSchema, createUserSchema, updateUserSchema };
