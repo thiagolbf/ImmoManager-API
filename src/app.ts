253806863e8d63dc102c4786bd8e30640fa3cbfb
Thiago Barcelos <thiagolbf@hotmail.com>
2023-08-25 20:00:40 -0300

    feat: add userRouter in app

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,12 @@ import { handleErrors } from "./errors";
 
 import express, { Application, json } from "express";
 
+import userRouter from "./routers/user.router";
+
 const app: Application = express();
 app.use(json());
+
+app.use("/user", userRouter);
 
 app.use(handleErrors);
 export default app;
