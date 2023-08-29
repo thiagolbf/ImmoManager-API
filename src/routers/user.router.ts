4c8b39fe152e63836e95f7b856c698640c8932ac
Thiago Barcelos <thiagolbf@hotmail.com>
2023-08-28 21:54:49 -0300

    add user service and controller

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
 
+import { validateBody } from "../middlewares/validateBody.middleware";
+
+import { createUserSchema } from "../schemas/user.schema";
+
+import { createUserController } from "../controllers/user.controller";
+
 const userRouter: Router = Router();
+
+userRouter.post("", validateBody(createUserSchema), createUserController);
 
 export default userRouter;
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const userSchema = z.object({
   id: z.number().positive(),
   name: z.string().max(50),
-  email: z.string().max(45),
+  email: z.string().max(45).email(),
   password: z.string().max(120),
   admin: z.boolean().default(false),
 });
