eb50540c9c69acedcfb238e4727d3b746455fe6f
Thiago Barcelos <thiagolbf@hotmail.com>
2023-08-30 20:48:20 -0300

    fix: category type

--- src/interfaces/category.interfaces.ts
+++ src/interfaces/category.interfaces.ts
@@ -3,6 +3,6 @@ import { createCategorySchema } from "../schemas/category.schema";
 import { Category } from "../entities/category.entity";
 
 type CategoryRequest = z.infer<typeof createCategorySchema>;
-type CategoryRead = Array<Category>;
+type CategoryRead = Category;
 
 export { CategoryRequest, CategoryRead };
